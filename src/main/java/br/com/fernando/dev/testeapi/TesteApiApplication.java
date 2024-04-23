51cdd300e69c700e91f282a80fe6213feb7dd47a
Fernando Silveira <appsuporte.silveira@gmail.com>
2024-04-23 20:39:48 -0300

    Inserção arquivos minimos API

--- src/main/java/br/com/fernando/dev/testeapi/TesteApiApplication.java
+++ src/main/java/br/com/fernando/dev/testeapi/TesteApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TesteApiApplication {
 
+//teste
 	public static void main(String[] args) {
 		SpringApplication.run(TesteApiApplication.class, args);
 	}
